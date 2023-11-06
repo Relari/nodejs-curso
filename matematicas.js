b01b39cefb0e728420e9e429e95004de7546a137
Relari <renzolavador@gmail.com>
2023-11-05 20:19:19 -0500

    exportando funciones o modulos

--- matematicas.js
+++ matematicas.js
@@ -1,3 +1,5 @@
+const Math = {};
+
 function add(number_one, number_two) {
     return number_one + number_two;
 }
@@ -18,8 +20,26 @@ function divide(number_one, number_two) {
     }
 }
 
+/**
 console.log(add(1,2));
 console.log(subtract(1,2));
 console.log(multiply(1,2));
 console.log(divide(1,2));
 console.log(divide(1,0));
+ */
+
+// Primera forma de exportar funciones.
+/*
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
+*/
+
+// Segunda forma de exportar modulo.
+Math.add = add;
+Math.subtract = subtract;
+Math.multiply = multiply;
+Math.divide = divide;
+
+module.exports = Math;
